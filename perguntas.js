9c87bfd91893291305c31974a8164f2cd554dbd0
leonardo52006 <00001094233742sp@al.educacao.sp.gov.br>
2024-12-02 11:22:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Futebol',
+    'Qual clássico do futebol brasileiro é conhecido como Clássico dos Milhões?',
+    'A maior rivalidade do futebol brasileiro é entre Flamengo e Vasco, conhecida como Clássico dos Milhões'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual clássico paulista é chamado de Derby Paulista?',
+    'A maior rivalidade em São Paulo é entre Corinthians e Palmeiras, conhecida como Derby Paulista'
+)
+
+criaCartao(
+    'Futebol',
+    'Como é chamado o clássico entre Flamengo e Fluminense?',
+    'A maior rivalidade no Rio de Janeiro é entre Flamengo e Fluminense, conhecida como Fla-Flu'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o nome do clássico entre Grêmio e Internacional?',
+    'A maior rivalidade no Rio Grande do Sul é entre Grêmio e Internacional, conhecida como Gre-Nal'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual clássico mineiro envolve Atlético-MG e Cruzeiro?',
+    'A maior rivalidade em Minas Gerais é entre Atlético-MG e Cruzeiro, conhecida como o Clássico Mineiro'
+)
